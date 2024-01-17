a0603bdd911eec5fb75d0a119e7e958f72804c57
khoai <khanhphuongdo28@gmail.com>
2024-01-17 11:38:58 -0500

    Main game loop initial

--- main.py
+++ main.py
@@ -11,3 +11,24 @@ HEIGHT = 600
 TITLE = "Falling Object Game"
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption(TITLE)
+
+# Clock
+clock = pygame.time.Clock()
+
+# Main Game Loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    # Drawing
+    
+    # Renew screen
+    pygame.display.flip()
+
+    # Frames per sec
+    clock.tick(30)
+
+# Exit game
+pygame.quit()
