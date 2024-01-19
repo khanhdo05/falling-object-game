5296f1b8312f5a06a6e1bfc3b6ca2379227b31c4
khoai <khanhphuongdo28@gmail.com>
2024-01-19 12:06:27 -0500

    screen

--- main.py
+++ main.py
@@ -98,6 +98,7 @@ while running:
 
 # Game Over Screen
 game_over_color = (26, 29, 33) # Black
+screen.fill(game_over_color)
 game_over_text = font.render("GAME OVER. SCORE: " + str(score), True, (245, 240, 230))
 screen.blit(game_over_text, (WIDTH // 2 - 100, HEIGHT // 2))
 pygame.display.flip()
