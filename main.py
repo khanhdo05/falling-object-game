fa83eab2f51b45931e94a1238b8994018fbcdec2
khoai <khanhphuongdo28@gmail.com>
2024-01-19 15:27:32 -0500

    add background image and change the ground height

--- main.py
+++ main.py
@@ -18,6 +18,8 @@ background_color = (252, 227, 252) # Light Pink
 object_color = (69, 143, 247)      # Blue
 player_color = (209, 27, 115)      # Hot Pink
 
+background_image = pygame.image.load('graphics/background.png')
+
 # Falling object's properties
 object_size = WIDTH // 15
 object_x = random.randint(0, WIDTH - object_size)
@@ -27,12 +29,13 @@ object_speed = 20
 # Player's properties
 player_size = WIDTH // 15
 player_x = WIDTH // 2           # middle
-player_y = HEIGHT - player_size # ground
+player_y = HEIGHT - player_size - 166 # ground
 
 # Other
 clock = pygame.time.Clock()
 score = 0
 pixel_font = pygame.font.Font('font/VT323/VT323-Regular.ttf', 200)
+regular_font = pygame.font.Font('font/Roboto/Roboto-Medium.ttf', 100)
 
 # Main Game Loop
 
@@ -60,16 +63,16 @@ while running:
                 running = False
     
     # Draws a new screen
-    screen.fill(background_color)
+    screen.blit(background_image, (0, 0))
 
     # Update object's position
     if is_paused == False:
         object_y += object_speed
-        if object_y >= HEIGHT:
+        if object_y >= HEIGHT - player_size - 166:
             running = False
     
     # Collision Check
-    if player_y < object_y + object_size and object_x < player_x + player_size and player_x < object_x + object_size:
+    if player_y + 166 < object_y + object_size and object_x < player_x + player_size and player_x < object_x + object_size:
         score += 1
         object_y = 0
         object_x = random.randint(0, WIDTH - object_size)      
@@ -82,10 +85,10 @@ while running:
 
     # Text
     if is_paused == False:
-        text = pixel_font.render("Score: " + str(score), True, (0, 0, 0))
+        text = regular_font.render("Score: " + str(score), True, (0, 0, 0))
         screen.blit(text, (70, 70))
     else:
-        text_paused = pixel_font.render("Paused. Press Space to Resume!", True, (0, 0, 0))
+        text_paused = regular_font.render("Paused. Press Space to Resume!", True, (0, 0, 0))
         screen.blit(text_paused, (WIDTH // 2 - (WIDTH // 3), HEIGHT // 2))
 
     # Drawing
@@ -101,8 +104,8 @@ while running:
 # Game Over Screen
 game_over_color = (15, 20, 31) # Dark Blue
 screen.fill(game_over_color)
-game_over_text = pixel_font.render("WILL YOU BE MY", True, (252, 43, 113))
-press_to_quit = pixel_font.render("VALENTINE?", True, (252, 43, 113))
+game_over_text = pixel_font.render("XXXXXXXXXXXXX", True, (252, 43, 113))
+press_to_quit = pixel_font.render("XXXXXXXXXX", True, (252, 43, 113))
 screen.blit(game_over_text, (WIDTH // 2 - 550, HEIGHT // 2 - 200))
 screen.blit(press_to_quit, (WIDTH // 2 - 400, HEIGHT // 2))
 pygame.display.flip()
